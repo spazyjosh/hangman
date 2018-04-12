7e69305d3d33a3c58ed97e20b0c29cae723a7fda
spazyjosh <joshuajacob1@comcast.net>
2018-04-12 16:52:17 -0600

    Update Hangman.java

--- Hangman.java
+++ Hangman.java
@@ -12,7 +12,11 @@ import java.util.Scanner;
  *
  * @author joshu
  */
+
 public class Hangman {
+     public static int wrongCounter = 0;{}
+    
+   // public static int wrongCounter = 0;
 
     /**
      * @param args the command line arguments
@@ -20,8 +24,13 @@ public class Hangman {
     public static void main(String[] args) {
         Random myRandom = new Random();
         Scanner scnr = new Scanner(System.in);
+      //3
+      HangMethods hang = new HangMethods();
         int difficulty;
-        int wrongCounter = 0;
+      WordSelector wordy = new WordSelector();
+        
+       
+       
         int lettersPrinted = 0;
         boolean keepGoing = false;
         String mysteryWord = "";
@@ -30,15 +39,13 @@ public class Hangman {
         String[] threeLetterWords = {"axe", "bum", "dog", "elk", "fog", "ham", "ion", "men", "nun", "cat", "run", "rid", "dig", "kid"};
         String[] fourLetterWords = {"help", "love", "luck", "muck", "joke", "poop", "kill", "find", "mind", "read", "shoe", "hair", "dogs", "clap", "hard", "soft", "rest", "nest", "best", "cool"};
 
-        do {
-            System.out.print("Please select either difficulty 3 or 4: ");
-            try {
-                difficulty = scnr.nextInt();
-            } catch (Exception e) {
-                System.out.println("Please insert a number");
-                difficulty = 0;
-            }
+        
+        
+            System.out.print("Please select either word length 3 or 4: ");
+            wordy.setWordLength(scnr.nextInt());
             scnr.nextLine();
+            wordy.decideWord();
+            /*
             switch (difficulty) {
                 case 3:
                     mysteryWord = threeLetterWords[myRandom.nextInt(threeLetterWords.length - 1)];
@@ -55,31 +62,34 @@ public class Hangman {
                     keepGoing = true;
                     break;
             }
-        } while (keepGoing);
-        char[] wordArray = new char[difficulty];
+        } while (keepGoing); */
+        char[] wordArray = new char[wordy.getWordLength()];
 
-        while (wrongCounter < (difficulty + 3)) {
+        while (wrongCounter < 7) {
             charsPrinted = 0;
-            if (lettersPrinted == difficulty) {
-                System.out.printf("Congratlations! You win! The word was %s.\n", mysteryWord);
+            if (lettersPrinted == wordy.getWordLength()) {
+                System.out.printf("Congratlations! You win! The word was %s.\n", wordy.getMysteryWord());
                 break;
             }
             System.out.print("Please enter a guess: ");
+            
             String currentGuess = scnr.nextLine();
             lettersPrinted = 0;
             previousGuesses.add(currentGuess.charAt(0));
-            if (mysteryWord.contains(currentGuess)) {
-                if (mysteryWord.equals(currentGuess)) {
-                    System.out.println("Correct! The word was " + mysteryWord + ". You win!");
+            if (wordy.getMysteryWord().contains(currentGuess)) {//hang.hangHim();
+                
+                if (wordy.getMysteryWord().equals(currentGuess)) {
+                 
+                    System.out.println("Correct! The word was " + wordy.getMysteryWord() + ". You win!");
                     break;
 
                 }
-                wordArray = mysteryWord.toCharArray();
+                wordArray = wordy.getMysteryWord().toCharArray();
 
-                for (int i = 0; i < difficulty; i++) {
+                for (int i = 0; i < wordy.getWordLength(); i++) {
 
                     if (previousGuesses.contains(wordArray[i])) {
-                        if (charsPrinted == difficulty) {
+                        if (charsPrinted == wordy.getWordLength()) {
                             break;
                         }
 
@@ -90,23 +100,33 @@ public class Hangman {
                         lettersPrinted++;
 
                     } else {
-                        if (charsPrinted == difficulty) {
+                        if (charsPrinted == wordy.getWordLength()) {
                             break;
                         }
 
                         System.out.print("__ ");
                         charsPrinted++;
                     }
+                    
 
                 }
                 System.out.println();
+                //hang.hangHim();
+                //System.out.println();
             } //need to add picture drawing for Wrong guesses-jj
             else {
-                System.out.print("WRONG");
                 wrongCounter++;
-                System.out.printf(" You have %d more chances\n", (difficulty + 3) - wrongCounter);
-                if (wrongCounter == difficulty + 3) {
-                    System.out.printf("Sorry, the word was %s\n", mysteryWord);
+                //hang.hangHim();
+                System.out.print("WRONG");
+                System.out.printf(" You have %d more chances\n", 7 - wrongCounter);
+                
+                if (wrongCounter == 7) {
+                    System.out.printf("Sorry, the word was %s\n", wordy.getMysteryWord());
+                    //hang.hangHim();
+                }
+                else
+                {
+                   // hang.hangHim();
                 }
 
             }
