a0e39ab16a59c5476f6b6b81977e3c7871dce999
spazyjosh <joshuajacob1@comcast.net>
2018-04-18 17:57:42 -0600

    Update Hangman.java

--- Hangman.java
+++ Hangman.java
@@ -26,7 +26,7 @@ public class Hangman {
         Scanner scnr = new Scanner(System.in);
       //3
       HangMethods hang = new HangMethods();
-        int difficulty;
+    
       WordSelector wordy = new WordSelector();
         
        
@@ -36,36 +36,20 @@ public class Hangman {
         String mysteryWord = "";
         ArrayList<Character> previousGuesses = new ArrayList<>();
         int charsPrinted = 0;
-        String[] threeLetterWords = {"axe", "bum", "dog", "elk", "fog", "ham", "ion", "men", "nun", "cat", "run", "rid", "dig", "kid"};
-        String[] fourLetterWords = {"help", "love", "luck", "muck", "joke", "poop", "kill", "find", "mind", "read", "shoe", "hair", "dogs", "clap", "hard", "soft", "rest", "nest", "best", "cool"};
-
+        
         
         
             System.out.print("Please select either word length 3 or 4: ");
             wordy.setWordLength(scnr.nextInt());
             scnr.nextLine();
             wordy.decideWord();
-            /*
-            switch (difficulty) {
-                case 3:
-                    mysteryWord = threeLetterWords[myRandom.nextInt(threeLetterWords.length - 1)];
-                    keepGoing = false;
-                    break;
-
-                case 4:
-                    mysteryWord = fourLetterWords[myRandom.nextInt(fourLetterWords.length - 1)];
-                    keepGoing = false;
-                    break;
-
-                default:
-                    System.out.println("Value out of range.");
-                    keepGoing = true;
-                    break;
-            }
-        } while (keepGoing); */
+            System.out.print("Please enter the amount of chances you would like: ");
+            int chances = scnr.nextInt();
+            scnr.nextLine();
+       
         char[] wordArray = new char[wordy.getWordLength()];
 
-        while (wrongCounter < 7) {
+        while (wrongCounter < chances) {
             charsPrinted = 0;
             if (lettersPrinted == wordy.getWordLength()) {
                 System.out.printf("Congratlations! You win! The word was %s.\n", wordy.getMysteryWord());
@@ -116,11 +100,11 @@ public class Hangman {
             } //need to add picture drawing for Wrong guesses-jj
             else {
                 wrongCounter++;
-                //hang.hangHim();
+               // hang.hangHim();
                 System.out.print("WRONG");
-                System.out.printf(" You have %d more chances\n", 7 - wrongCounter);
+                System.out.printf(" You have %d more chances\n", chances - wrongCounter);
                 
-                if (wrongCounter == 7) {
+                if (wrongCounter == chances) {
                     System.out.printf("Sorry, the word was %s\n", wordy.getMysteryWord());
                     //hang.hangHim();
                 }
