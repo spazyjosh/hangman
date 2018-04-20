0a938e91055204328d28e46ba8139b40ed7e2f8d
spazyjosh <joshuajacob1@comcast.net>
2018-04-19 19:01:10 -0600

    Update Hangman.java

--- Hangman.java
+++ Hangman.java
@@ -61,6 +61,101 @@ public class Hangman {
                 System.out.printf("Congratlations! You win! The word was %s.\n", wordy.getMysteryWord());
                 break;
             }
+            if(previousGuesses.size()>0)
+            {
+            if(wordy.getMysteryWord().contains(String.format("%c", (char)previousGuesses.get(previousGuesses.size()-1))))
+                System.out.println();
+            }
+            
+            hang.hangHim();
+            System.out.print("Please enter a guess: ");
+            
+            String currentGuess = scnr.nextLine();
+            lettersPrinted = 0;
+            previousGuesses.add(currentGuess.charAt(0));
+           // if (wordy.getMysteryWord().contains(currentGuess)) {//hang.hangHim();
+                
+                if (wordy.getMysteryWord().equals(currentGuess)) {
+                 
+                    System.out.println("Correct! The word was " + wordy.getMysteryWord() + ". You win!");
+                    break;
+
+                }
+                if(wordy.getMysteryWord().contains(currentGuess)==false) {
+                wrongCounter++;
+               //hang.hangHim();
+                System.out.print("\nWRONG");
+                System.out.printf(" You have %d more chances\n", chances - wrongCounter);
+                
+                if (wrongCounter == chances) {
+                    System.out.printf("Sorry, the word was %s\n", wordy.getMysteryWord());
+                    //hang.hangHim();
+                }
+            }
+                wordArray = wordy.getMysteryWord().toCharArray();
+
+                for (int i = 0; i < wordy.getWordLength(); i++) {
+
+                    if (previousGuesses.contains(wordArray[i])) {
+                        if (charsPrinted == wordy.getWordLength()) {
+                            System.out.println();
+                            break;
+                        }
+
+                        System.out.print(wordArray[i] + " ");
+
+                        charsPrinted++;
+
+                        lettersPrinted++;
+
+                    } else {
+                        if (charsPrinted == wordy.getWordLength()) {
+                            System.out.println();
+                            break;
+                        }
+
+                        System.out.print("__ ");
+                        charsPrinted++;
+                    }
+                    
+
+                }
+                
+                //System.out.println();
+                //hang.hangHim();
+                //System.out.println();
+           // } //need to add picture drawing for Wrong guesses-jj
+            /*if(wordy.getMysteryWord().contains(currentGuess)==false) {
+                wrongCounter++;
+               //hang.hangHim();
+                System.out.print("\nWRONG");
+                System.out.printf(" You have %d more chances\n", chances - wrongCounter);
+                
+                if (wrongCounter == chances) {
+                    System.out.printf("Sorry, the word was %s\n", wordy.getMysteryWord());
+                    //hang.hangHim();
+                }
+                
+
+            }*/
+
+        }
+
+    }
+}
+            wordy.decideWord();
+            System.out.print("\nPlease enter the amount of chances you would like: ");
+            int chances = scnr.nextInt();
+            scnr.nextLine();
+       
+        char[] wordArray = new char[wordy.getWordLength()];
+
+        while (wrongCounter < chances) {
+            charsPrinted = 0;
+            if (lettersPrinted == wordy.getWordLength()) {
+                System.out.printf("Congratlations! You win! The word was %s.\n", wordy.getMysteryWord());
+                break;
+            }
             System.out.print("Please enter a guess: ");
             
             String currentGuess = scnr.nextLine();
